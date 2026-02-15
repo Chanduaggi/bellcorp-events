f69562180528a68822f7895162d8a17c52b17df9
Aggi Chandu <aggichandu@gmail.com>
2026-02-15 23:37:10 +0530

    Update CORS for production

--- server/server.js
+++ server/server.js
@@ -15,7 +15,10 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://bellcorp-events-three.vercel.app/",
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
